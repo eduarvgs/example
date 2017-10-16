6136f3904fa6105b942162cd3b09b68cad9f2993
jperezv16 <jperezvelazco@gmail.com>
2017-10-16 13:48:24 -0700

    calendar.php

--- parts/calendar.php
+++ parts/calendar.php
@@ -1,1 +1,155 @@
-ydtrrdd
+<div class="select">
+  <select>
+    <option>Select project</option>
+    <option>Ponderosa</option>
+    <option>puente e' hierro</option>
+    <option>Tronconal</option>
+    <option>Pelaez</option>
+    <option>las casitas</option>
+    <option>plateau</option>
+    <option>buggy</option>
+    <option>mono cuicui</option>
+
+  </select>
+</div>
+
+<table class="table">
+    <thead>
+        <tr>
+            <th><abbr title="Position">Hour</abbr></th>
+            <th><abbr title="Monday">Mon</abbr></th>
+            <th><abbr title="Tuesday">Tue</abbr></th>
+            <th><abbr title="Wednesday">Wen</abbr></th>
+            <th><abbr title="Thursday">thu</abbr></th>
+            <th><abbr title="Friday">Fri</abbr></th>
+            <th><abbr title="Saturday">Sat</abbr></th>
+            <th><abbr title="Sunday">Sun</abbr></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+              7:00 am
+              -
+              12m
+            </td>
+            <td class="content is-small">
+                <ul>
+                    <li>
+                        Eduar Vegas
+                    </li>
+                    <li>
+                        Jose Perez
+                    </li>
+                    <li>
+                        Gustavo
+                    </li>
+                </ul>
+                <a class="button is-small">Small</a>
+            </td>
+            <td class="content is-small">
+                <ul>
+                    <li>
+                        Jose Perez
+                    </li>
+                    <li>
+                        Gustavo
+                    </li>
+                </ul>
+            </td>
+            <td class="content is-small">
+                <ul>
+                    <li>
+                        Eduar Vegas
+                    </li>
+                    <li>
+                        Gustavo
+                    </li>
+                </ul>
+            </td>
+
+            <td class="content is-small">
+                <ul>
+                    <li>
+                        Juan Perez
+                    </li>
+                    <li>
+                        Gustavo
+                    </li>
+                </ul>
+            </td>
+            <td class="content is-small">
+                <ul>
+                    <li>
+                        Eduar Vegas
+                    </li>
+                    <li>
+                        Pedro
+                    </li>
+                </ul>
+            </td>
+            
+        </tr>
+        <tr>
+            <td>
+              1:00 pm
+              -
+              6:00 pm
+            </td>
+                    <td class="content is-small">
+                <ul>
+                    <li>
+                        Eduar Vegas
+                    </li>
+                    <li>
+                        Jose Perez
+                    </li>
+                    <li>
+                        Gustavo
+                    </li>
+                </ul>
+            </td>
+            <td class="content is-small">
+                <ul>
+                    <li>
+                        Jose Perez
+                    </li>
+                    <li>
+                        Gustavo
+                    </li>
+                </ul>
+            </td>
+            <td class="content is-small">
+                <ul>
+                    <li>
+                        Eduar Vegas
+                    </li>
+                    <li>
+                        Gustavo
+                    </li>
+                </ul>
+            </td>
+
+            <td class="content is-small">
+                <ul>
+                    <li>
+                        Juan Perez
+                    </li>
+                    <li>
+                        Gustavo
+                    </li>
+                </ul>
+            </td>
+            <td class="content is-small">
+                <ul>
+                    <li>
+                        Eduar Vegas
+                    </li>
+                    <li>
+                        Pedro
+                    </li>
+                </ul>
+            </td>
+        </tr>    
+    </tbody>
+</table>
