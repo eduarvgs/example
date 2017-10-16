e4b8aa767b80cdb784b6556c00ed57fb1d8202db
jperezv16 <jperezvelazco@gmail.com>
2017-10-15 20:06:07 -0700

    update sched

---
+++ schedule.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.2/css/bulma.min.css">
+
+        <title>
+            Project Eduar Vegas
+        </title>
+        
+      </head>
+      <body>
+  
+      <?php 
+          include('parts/navbar.php');
+
+          $title='Schedule';  
+          include('parts/hero.php');
+
+          include('parts/calendar.php');
+      
+          include('parts/footer.php');
+      ?>  
+
+  </body>
+</html>
